76340616994a2b06280ca5ab2d3148d4820913d0
camilabondar <camilabondar@gmail.com>
2013-07-20 19:01:40 -0300

    Update web.js

--- web.js
+++ web.js
@@ -1,20 +1,16 @@
-var fs = require("fs");
 var express = require('express');
+var fs = require('fs');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
-//app.get('/', function(request, response) {
-//  response.send('Helloafsdfasdfasd!');
-
-var buf = fs.readFileSync("index.html");
-var index = buf.toString();
 
 app.get('/', function(request, response) {
-    response.send(index);
-
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
